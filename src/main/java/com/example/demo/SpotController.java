c4ed5983b3f1fd1f67ee16e93f543e977ac20aae
Brian Jackson <brianjackson1224@gmail.com>
2021-08-31 14:07:20 -0700

    Added links to Spot api returns

--- src/main/java/com/example/demo/SpotController.java
+++ src/main/java/com/example/demo/SpotController.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,8 +25,13 @@ public class SpotController {
 	}
 	
 	@GetMapping("/spots")
-	List<Spot> all() {
-		return repository.findAll();
+	CollectionModel<EntityModel<Spot>> all() {
+		List<EntityModel<Spot>> spots = repository.findAll().stream().map(spot -> EntityModel.of(spot, 
+				linkTo(methodOn(SpotController.class).one(spot.getId())).withSelfRel(),
+				linkTo(methodOn(SpotController.class).all()).withRel("spots")
+				)).collect(Collectors.toList());
+		
+		return CollectionModel.of(spots, linkTo(methodOn(SpotController.class).all()).withSelfRel());
 	}
 	
 	@PostMapping("/spots")
@@ -30,10 +40,15 @@ public class SpotController {
 	}
 	
 	@GetMapping("/spots/{id}")
-	Spot one(@PathVariable Long id) {
+	EntityModel<Spot> one(@PathVariable Long id) {
 		
-		return repository.findById(id)
+		Spot spot = repository.findById(id)
 				.orElseThrow(() -> new SpotNotFoundException(id));
+		
+		return EntityModel.of(spot,
+				linkTo(methodOn(SpotController.class).one(id)).withSelfRel(),
+				linkTo(methodOn(SpotController.class).all()).withRel("spots")
+				);
 	}
 	
 	@PutMapping("spots/{id}")
