6a6b646d0ff86c40df8f38ebb3d6c2cf11247fc6
Brian <brianjackson1224@gmail.com>
2021-08-31 12:24:43 -0700

    Added RESTful links to User api calls

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,8 +24,13 @@ public class UserController {
 	}
 	
 	@GetMapping("/users")
-	public List<User> all() {
-		return repository.findAll();
+	CollectionModel<EntityModel<User>> all() {
+		List<EntityModel<User>> users = repository.findAll().stream().map(user -> EntityModel.of(user,
+				linkTo(methodOn(UserController.class).one(user.getId())).withSelfRel(),
+				linkTo(methodOn(UserController.class).all()).withRel("users")
+				)).collect(Collectors.toList());
+		
+		return CollectionModel.of(users, linkTo(methodOn(UserController.class).all()).withSelfRel());
 	}
 	
 	@PostMapping("/users")
@@ -29,9 +39,14 @@ public class UserController {
 	}
 	
 	@GetMapping("/users/{id}")
-	User one(@PathVariable Long id) {
-		return repository.findById(id)
+	EntityModel<User> one(@PathVariable Long id) {
+		User user = repository.findById(id)
 				.orElseThrow(() -> new UserNotFoundException(id));
+		
+		return EntityModel.of(user, 
+				linkTo(methodOn(UserController.class).one(id)).withSelfRel(),
+				linkTo(methodOn(UserController.class).all()).withRel("users")
+				);
 	}
 	
 	@PutMapping("/users/{id}")
